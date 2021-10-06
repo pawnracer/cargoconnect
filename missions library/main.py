83455043d37de1a92130e9c1a0ee0be778826149
Sushimaker5633 <66091837+sushimaker5633@users.noreply.github.com>
2021-10-06 16:16:31 -0700

    Create main.py

---
+++ missions library/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from missions import Missions
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+gs=GyroSensor(Port.S4)
+lm=Motor(Port.B)
+rm=Motor(Port.C)
+m=Missions(gyro=gs, leftMotor=lm, rightMotor=rm, inverted=True)
+
+# Write your program here.
+m.test()
