856a7e8e53be0bcd9b8dcb4c8eefb4116d6cd11b
Sushimaker5633 <66091837+Sushimaker5633@users.noreply.github.com>
2021-10-20 15:46:14 -0700

    Update missions.py

--- missions library/missions.py
+++ missions library/missions.py
@@ -3,7 +3,13 @@
 """
 This program uses the basic driving functions to do missions. Add more functions as needed.
 """
-
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 from base import baseMovement
 
 
