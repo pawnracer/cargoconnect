9cb716bf90817f250cd4e204df1d5c848092e989
pawnracer <90972605+pawnracer@users.noreply.github.com>
2021-10-20 15:24:41 -0700

    Update base.py

--- missions library/base.py
+++ missions library/base.py
@@ -2,13 +2,6 @@
 """
 This program contains basic gyro driving function such as gyroTankTurn and gyroDriveCm.
 """
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 class baseMovement(object):
     def __init__(self, gyro, leftMotor, rightMotor, inverted):
         """
